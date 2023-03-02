f21e408317b3deb72ab3b3657657770f93339da9
R00bin800 <72667588+R00bin800@users.noreply.github.com>
2023-03-02 20:15:13 +0100

    Add files via upload

--- pages/01_random waves generator.py
+++ pages/01_random waves generator.py
@@ -23,22 +23,23 @@ max_height = st.number_input("maxiumum wave height",min_value=min_height, max_va
 min_period = st.number_input("minimum wave period",min_value=1,max_value=119)
 max_period = st.number_input("maxiumum wave period",min_value=min_period, max_value=120)
 
-if st.button("⚙️ Generate!"):
-    for i in range(amount):
-        waves.append(random.randint(min_height,max_height))
-        period.append(random.randint(min_period,max_period))
+if amount != 0:
+    if st.button("⚙️ Generate!"):
+        for i in range(amount):
+            waves.append(random.randint(min_height,max_height))
+            period.append(random.randint(min_period,max_period))
 
-    df = pd.DataFrame(list(zip(waves,period)),columns = ["golfhoogte", "golfperiode"]).transpose()
-    df = df[df.columns[0:]].apply(lambda x: ', '.join(x.dropna().astype(str)),axis=1)
-    def to_excel(df):
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df.to_excel(writer, index=False, header=False, sheet_name='Sheet1')
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-        format1 = workbook.add_format({'num_format': '0'}) 
-        worksheet.set_column('A:A', None, format1)  
-        writer.save()
-        processed_data = output.getvalue()
-        return processed_data
-    st.download_button("📥 Download data!", data=to_excel(df),file_name="wavetestdata.xlsx")
+        df = pd.DataFrame(list(zip(waves,period)),columns = ["golfhoogte", "golfperiode"]).transpose()
+        df = df[df.columns[0:]].apply(lambda x: ', '.join(x.dropna().astype(str)),axis=1)
+        def to_excel(df):
+            output = BytesIO()
+            writer = pd.ExcelWriter(output, engine='xlsxwriter')
+            df.to_excel(writer, index=False, header=False, sheet_name='Sheet1')
+            workbook = writer.book
+            worksheet = writer.sheets['Sheet1']
+            format1 = workbook.add_format({'num_format': '0'}) 
+            worksheet.set_column('A:A', None, format1)  
+            writer.save()
+            processed_data = output.getvalue()
+            return processed_data
+        st.download_button("📥 Download data!", data=to_excel(df),file_name="wavetestdata.xlsx")
