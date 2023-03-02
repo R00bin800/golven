489615c33a05cf07a61b432e9b32411b4ac7b522
R00bin800 <72667588+R00bin800@users.noreply.github.com>
2023-03-02 20:12:41 +0100

    Add files via upload

--- pages/02_wave analysis.py
+++ pages/02_wave analysis.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import statistics as stat
 import streamlit as st
 
 st.set_page_config(page_title="Wave analysis")
@@ -25,16 +24,15 @@ if uploaded:
     g_hoogte_list = df["Golfhoogte"].tolist()
     g_periode_list = df["Golfperiode"].tolist()
 
-    height_average = stat.fmean(g_hoogte_list)                      #get the average height from the hoogte list
-    period_average = stat.fmean(g_periode_list)                     #get the average period from the periode list
+    height_average = np.mean(g_hoogte_list)                      #get the average height from the hoogte list
+    period_average = np.mean(g_periode_list)                     #get the average period from the periode list
 
-    Hs_list = [i for i in g_hoogte_list if i > np.percentile(g_hoogte_list, 66.666667)]
-    Hs = stat.fmean(Hs_list)                                        #get the average height from the Hs_list, this is the significant wave height
+    Hs_list = [i for i in g_hoogte_list if i >= np.percentile(g_hoogte_list, 66.666667)]
+    Hs = np.mean(Hs_list)                                        #get the average height from the Hs_list, this is the significant wave height
 
     index = [i for i, j in enumerate(g_hoogte_list) if j in Hs_list]          #get the location of the significant waves in the orignial wave list.
     g_period_Hs = [g_periode_list[i] for i in index]    
-    Tp = stat.fmean(g_period_Hs)                                    #Find the average wave period for the significant waves
-
+    Tp = np.mean(g_period_Hs)                                    #Find the average wave period for the significant waves
 
     max_height = max(g_hoogte_list)                                 #get the highest wave and its corresponding peroid
     max_corresponding_period = g_periode_list[g_hoogte_list.index(max_height)]
@@ -44,7 +42,6 @@ if uploaded:
 
     longest_period = max(max_corresponding_period, min_corresponding_period)        #find the longest period between the lowest and highest wave
 
-
     x = np.arange(0,longest_period,0.1)                                             #store the values of 0.1 steps between 0 and the longest_period
     y = 0.5*max_height*np.sin((2*np.pi/max_corresponding_period)*x)                 #plot the highest wave
     z = 0.5*min_height*np.sin((2*np.pi/min_corresponding_period)*x)                 #plot the lowest wave
@@ -52,27 +49,52 @@ if uploaded:
 
     steps = np.arange(0, longest_period, step=np.pi / 2)
     label = [str(j) + 'π' for j in np.arange(0, steps.size * 0.5, 0.5)]
+    plt.rc('axes', axisbelow=True)
 
-    fig = plt.figure()
+    fig = plt.figure(0)
     plt.xlabel('time (s)')  # label on the x plane
     plt.ylabel('height (m)') #label on the y plane
     plt.title('Plot of wave height from 0 to %i seconds' %longest_period)   #title of the graph
     plt.grid(True, which='both')                                            #show the grid
     plt.plot(x,y,x,z,x,w)                                                   #plot everything for real
-    plt.gca().legend(('Highest wave', 'lowest wave', 'combined waves'))      #titles of each graph
-    plt.xticks(steps, label, rotation = 45)                                                #show those value of pi
-    plt.savefig("myplot.png", dpi = 'figure')
-    plt.show()
-
+    plt.gca().legend(('Highest wave', 'lowest wave', 'combined waves'))     #titles of each graph
+    plt.xticks(steps, label, rotation = 45)                                 #show those value of pi
     st.pyplot(fig)
+
+    normal_wave = [i for j, i in enumerate(g_hoogte_list) if j not in index]
+    normal_wave_period = [i for j, i in enumerate(g_periode_list) if j not in index]
+    fig2 = plt.figure(1)
+    plt.grid(True, which='both')
+    plt.grid(which='minor', alpha=0.2)    
+    plt.grid(which='major', alpha=0.5)
+    plt.scatter(x=normal_wave_period, y=normal_wave, c='b', label='Normal waves', alpha=0.2)
+    plt.scatter(x=g_period_Hs, y=Hs_list, c='r', label='Waves in Hs', alpha=0.2)
+    plt.scatter(Tp, Hs, c='g',label="Hs with Tp")
+    plt.xlabel('period (s)')  # label on the x plane
+    plt.ylabel('height (m)') #label on the y plane
+    plt.title('Waves used in calculation')   #title of the graph
+
+    xloc, xlabels = plt.xticks()
+    plt.xticks(np.arange(0, max(xloc), step=5))
+    plt.xticks(np.arange(0, max(xloc), step=1), minor=True)
+
+        
+    yloc, ylabels = plt.yticks()
+    plt.yticks(np.arange(0, max(yloc), step=5))   
+    plt.yticks(np.arange(0, max(yloc), step=1), minor=True)
+
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys(),labelspacing=0.1, loc='lower left')
+
+    st.pyplot(fig2)
+
     st.markdown(f"significante golfhoogte: {Hs:.2f} m")
     st.markdown(f"significante golfperiode: {Tp:.2f} sec")
     st.markdown(f"hoogste golf: {max_height:.2f} m")
     st.markdown(f"bijbehorende periode: {max_corresponding_period:.2f} s")
     st.markdown(f"laagste golf: {min_height:.2f} m")
     st.markdown(f"bijbehorende periode: {min_corresponding_period:.2f} s")
-
-
 
 
 
